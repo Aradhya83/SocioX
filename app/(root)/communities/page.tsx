d7b9d9a6dc82280947dff4889c00e0148fa3aeba
Aradhya83 <140255644+Aradhya83@users.noreply.github.com>
2023-11-12 01:47:53 +0530

    Update page.tsx

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,39 @@
-const Page = async() => {
-    return (
-        <section>
-            <h1 className="head-text mb-10">Communitites</h1>
-        </section>
-    )
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import Searchbar from "@/components/shared/Searchbar"
+
+
+import { fetchUser } from "@/lib/actions/user.actions";
+
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+ 
+
+  return (
+    <>
+      <h1 className='head-text'>Communities</h1>
+
+      <div className='mt-5'>
+        <Searchbar routeType='communities' />
+      </div>
+
+      
+          <p className='no-result flex align-left mt-6'>No Result</p>
+       
+    
+    </>
+  );
 }
 
 export default Page;
