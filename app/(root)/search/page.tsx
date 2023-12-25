6cf7eeb1c619ec03f03dd5d8451ea486736ed6d6
Aradhya83 <140255644+Aradhya83@users.noreply.github.com>
2023-12-26 01:43:30 +0530

    Update page.tsx

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,53 +1,57 @@
+import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
-import {redirect } from "next/navigation";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import Image from "next/image";
-import { profileTabs } from "@/constants";
-import ThreadsTab from "@/components/shared/ThreadsTab";
+
 import UserCard from "@/components/cards/UserCard";
 import Searchbar from "@/components/shared/Searchbar";
 
-async function Page(){
-    const user = await currentUser();
-    if(!user)return null;
 
-    const userInfo= await fetchUser(user.id);
-    if(!userInfo?.onboarded)redirect('/onbaording');
-    
-    const result= await fetchUsers({
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchUsers({
     userId: user.id,
-    searchString:'',
-    pageNumber: 1,
-    pageSize:25,
-   });
+    searchString: searchParams.q,
+    pageNumber:  1,
+    pageSize: 25,
+  });
 
-   return (
+  return (
     <section>
-        <h1 className='head-text mb-10'>Search</h1>
+      <h1 className='head-text mb-10'>Search</h1>
 
       <Searchbar routeType='search' />
-        <div className="mt-14 flex flex-col gap-9">
-                {result.users.length === 0 ?(
-                    <p className="no-result">No users found</p>
-                ): (
-                    <>
-                    {result.users.map((person)=> (
-                        <UserCard
-                        key={person.id}
-                        id={person.id}
-                        name={person.name}
-                        username={person.username}
-                        imgUrl={person.image}
-                        personType='User'
-                        />
-                    ))}
-                    </>
-                )}
-            </div>
-        
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
+
     </section>
-    );
+  );
 }
 
 export default Page;
