de4d26b8d06d8e13b1c3e1662becbb5d413b9830
Aradhya83 <140255644+Aradhya83@users.noreply.github.com>
2023-12-26 18:30:22 +0530

    Update RightSidebar.tsx

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
-import yourGif from '../shared/gifs/ezgif.com-crop (3).gif';
 
 const RightSidebar: React.FC = () =>{
     const styles: React.CSSProperties = {
@@ -11,14 +9,11 @@ const RightSidebar: React.FC = () =>{
       };
      
     return (
-        <section className="custom-scrollbar rightsidebar">
+        <section style={styles} className="custom-scrollbar rightsidebar">
             <div className="flex flex-1 flex-col justify-start">
-            <div className="text-light-1 flex flex-col" style={styles}>
+            <div className="text-light-1 flex flex-col" >
   <p>Shape your world with words! Express your unique thoughts, ignite connections, and be part of a global tapestry of ideas. Your voice matters – start a conversation, inspire, and make an impact through the written word.</p>
 </div>
-            </div>
-            <div className="flex flex-1 flex-col justify-start ml-10">
-<h3 className="text-heading4-medium text-light-1"> <Image style={styles} src={yourGif} alt="Your GIF" /></h3>
             </div>
         </section>
         )
