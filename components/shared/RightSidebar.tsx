3a00d4d9a0863202bcb30f29f1e9c966654f5732
Aradhya83 <140255644+Aradhya83@users.noreply.github.com>
2023-12-27 00:35:12 +0530

    Update RightSidebar.tsx

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 
-const RightSidebar: React.FC = () =>{
-    const styles: React.CSSProperties = {
-        maxWidth: '250px',
-        fontSize:'20px',
-        fontWeight: '450',
-        fontFamily: 'Georgia, serif',
-      };
-     
-    return (
-        <section style={styles} className="custom-scrollbar rightsidebar">
-            <div className="flex flex-1 flex-col justify-start">
-            <div className="text-light-1 flex flex-col" >
-  <p>Shape your world with words! Express your unique thoughts, ignite connections, and be part of a global tapestry of ideas. Your voice matters – start a conversation, inspire, and make an impact through the written word.</p>
-</div>
-            </div>
-        </section>
-        )
+const RightSidebar: React.FC = () => {
+  const styles: React.CSSProperties = {
+    maxWidth: '300px',
+    fontSize: '20px',
+    fontWeight: '450',
+    fontFamily: 'Georgia, serif',
+  };
+  const styless: React.CSSProperties = {
+    maxWidth: '300px',
+    fontSize: '27px',
+  
+    fontWeight: '450',
+    fontFamily: 'Georgia, serif',
+  };
+
+
+  return (
+    <section style={styles} className="custom-scrollbar w-full rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+      <div className="text-light-1 flex flex-col">
+         <h2  style= {styless}> HELLO THERE!</h2>
+        <br/>
+          <p>
+     Feel free to kickstart discussions, kindle innovative ideas, and groove with the most awesome minds out there! Be a trendsetter - MOLD conversations and SPARK inspiration.
+          </p>
+          <p>
+            So go ahead, make waves, and have fun leaving your mark on the canvas of boundless creativity! 
+           </p>
+        </div>
+    
+      
+      </div>
+    </section>
+  );
 }
 
 export default RightSidebar;
